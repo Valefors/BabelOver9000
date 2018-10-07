143701ad593155561c16f1bffd0119b5e7101dff
yesaa <debrab_c14@myges.fr>
2018-10-07 16:47:44 +0200

    animation cloud

---
+++ BabelOver9000/Assets/PlaceHolderAnim/SwitchAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchAnim : MonoBehaviour {
+
+	public Animation anim;
+	Animator totor;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animation> ();
+		totor = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+			anim.Play ();
+		//if (totor.GetCurrentAnimatorStateInfo (0).IsName ("AnimCloudTestToScreen"))
+		if (!anim.IsPlaying ("AnimCloudTestToScreen"))
+			print ("it's over");
+		else
+			print ("playing");
+	}
+}
