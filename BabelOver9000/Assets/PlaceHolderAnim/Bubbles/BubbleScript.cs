6a1d17fd2e8c6509cc418bf3ed700b48fc968493
yesaa <debrab_c14@myges.fr>
2018-10-07 18:03:54 +0200

    Bubble animation Test okay

---
+++ BabelOver9000/Assets/PlaceHolderAnim/Bubbles/BubbleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//script pour l'animation des bulles quand une note est validée
+//il faut juste faire la même chose avec un autre sprite (et probablement un autre nom) pour la bulle de note ratée
+
+public class BubbleScript: MonoBehaviour {
+
+	public Animation anim;
+	Animator totor;
+	public Sprite win;
+	public Sprite defaultSprite;
+
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animation> ();
+		totor = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.P))//condition de note réussie (à appeler du script, probablement)
+			anim.Play ();
+		if (!anim.IsPlaying ("BubbleAnimWin"))
+			print ("it's over");
+		else
+			print ("playing");
+	}
+}
